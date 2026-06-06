6e3fb0592b874084febed8ff75c09a8f367df225
Pa1rick101 <p.suehrer@gmail.com>
2026-06-06 13:39:14 +0200

    Use port 8000 by default

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,4 +11,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", "8000")))
